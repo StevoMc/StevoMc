7b6c5d7b8d87abd835386aa8b909b80b1b7b1aec
StevoMc <stevenmc59@gmail.com>
2021-10-24 05:14:37 +0200

    Changed App to use Typescript

--- steven_mcgough_portfolio/src/index.js
+++ steven_mcgough_portfolio/src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const props = {
+  name: "Steven McGough",
+  birthday: new Date(2001, 2, 31),
+  job: "Cyber Security Student",
+  country: "Germany",
+  data: [
+    {
+      title: "Benifits",
+      content: "Ich bin besser als alle!",
+    },
+    {
+      title: "Projects",
+      content: "Für die Uni eine Fullstack Webapp entwicket.",
+    },
+    {
+      title: "Process",
+      content: "Für die Uni eine Fullstack Webapp entwicket.",
+    },
+    {
+      title: "FAQ",
+      content: "Für die Uni eine Fullstack Webapp entwicket.",
+    },
+  ],
+};
+
+const test_props = {
+  name: "Moritz Petersen",
+  birthday: new Date(1980, 1, 1),
+  job: "Webflow Expert",
+  country: "Germany",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App {...props} />
+    {/* <App {...test_props} dev /> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
